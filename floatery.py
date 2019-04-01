dcbbf7b7813fd9fddb335301cd52fdab5c97a71d
Marco Westerhof <m.westerhof@lukkien.com>
2019-04-01 15:17:27 +0200

    initial commit

---
+++ floatery.py
@@ -0,0 +1,70 @@
+import neovim
+
+
+@neovim.plugin
+class FloateryPlugin:
+    default_size = ['90 40']
+
+    def __init__(self, nvim):
+        self.nvim = nvim
+        self.floatingWindow = None
+        self.floatingWindowConfig = {
+            'relative': 'editor',
+            'width': 50,
+            'height': 30,
+            'col': 0,
+            'row': 0,
+            'anchor': 'NW'
+        }
+
+    def log(self, message):
+        self.nvim.command(f'echom "{message}"')
+
+    @neovim.command('FloatIt', nargs='?')
+    def float_it(self, arg):
+        if not arg:
+            arg = self.default_size
+
+        width, height = [int(i) for i in arg[0].split()]
+
+        self._update_config(width, height)
+        if self.floatingWindow is None:
+            self._open_floating_window()
+        else:
+            self._update_floating_window()
+
+    @neovim.command('UnFloatIt')
+    def unfloat_it(self):
+        self.nvim.command_output(
+            f'call nvim_win_close({self.floatingWindow}, 0)')
+        self.floatingWindow = None
+
+    def _update_config(self, width, height):
+        width_editor = self.nvim.eval('&columns')
+        height_editor = self.nvim.eval('&lines')
+
+        self.floatingWindowConfig['width'] = width
+        self.floatingWindowConfig['height'] = height
+
+        col = (width_editor - self.floatingWindowConfig['width']) / 2
+        row = (height_editor - self.floatingWindowConfig['height']) / 2
+
+        self.floatingWindowConfig.update({'col': col, 'row': row})
+
+    def _open_floating_window(self):
+        buf = self.nvim.current.buffer
+        options = self.floatingWindowConfig
+        self.floatingWindow = self.nvim.eval(
+            f'nvim_open_win({buf.number}, 0, {options})'
+        )
+
+    def _update_floating_window(self):
+        options = self.floatingWindowConfig
+        window = self.floatingWindow
+        try:
+            self.nvim.eval(
+                f'nvim_win_set_config({window}, {options})'
+            )
+        except neovim.api.nvim.NvimError:
+            # ugly workaround, should just check if the damn window exists)
+            self._open_floating_window()
