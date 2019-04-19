dc108325342f24c87e796e2716fbf4a9334eb2ba
Marco Westerhof <m.westerhof@lukkien.com>
2019-04-19 11:38:28 +0200

    improve handling of hint clearing

--- linehint.py
+++ linehint.py
@@ -5,7 +5,7 @@ import neovim
 
 @neovim.plugin
 class LinehintPlugin:
-    hg_group = 'NonText'
+    hl_group = 'CursorLine'
 
     def __init__(self, nvim):
         self.nvim = nvim
@@ -14,12 +14,14 @@ class LinehintPlugin:
     def line_hist(self, arg):
         if arg:
             message = arg[0]
+            hl_group = self.hl_group
         else:
             message = ''
+            hl_group = 'Normal'
 
         _lineno = int(self.nvim.command_output("echo line('.')")) - 1
 
         self.nvim.command_output(
-            f"call nvim_buf_set_virtual_text(0, -1, {_lineno}, [['{message}',"
-            f"'{self.hg_group}']], {{}})"
+            f'call nvim_buf_set_virtual_text(0, -1, {_lineno}, [["{message}",'
+            f'"{hl_group}"]], {{}})'
         )
